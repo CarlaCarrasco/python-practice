af974a06d0b1337392c76efe4cbbd164a9ee9592
CarlaCarrasco <carlacarr37@gmail.com>
2021-05-27 07:06:47 -0500

    completed password-generator AND fizzbuzz

--- password-generator.py
+++ password-generator.py
@@ -1,18 +1,37 @@
 import random
 import string
 
-print('Welcome to the Password Generator!')
-
-letters = input('How many letters would you like in your password?')
-symbols = input('How many symbols would you like?')
-numbers = input('How many numbers would you like?')
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 randLetters = ''
 randNumbers = ''
+randSymbols = ''
 
-for x in range(letters):
+print('Welcome to the Password Generator!')
+
+numLetters = input('How many letters would you like in your password?')
+numSymbols = input('How many symbols would you like?')
+numbers = input('How many numbers would you like?')
+
+
+def shuffle_string():
+    s = randLetters + randNumbers + randSymbols
+    shuffleString = ''.join(random.sample(s, len(s)))
+    print(shuffleString)
+
+
+for x in range(numLetters):
     randLetters += random.choice(string.ascii_letters)
-    print(randLetters)
+print(randLetters)
+
+
+for x in range(numSymbols):
+    randSymbols += random.choice(symbols)
+print(randSymbols)
+
 
 for x in range(numbers):
     randNumbers += str(random.randint(0, 9))
-    print(randNumbers)
+print(randNumbers)
+
+
+shuffle_string()
